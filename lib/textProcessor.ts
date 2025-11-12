95753f99a949ac8fbddf9bb102433569a8f5e54a
elia <elia.weiss@gmail.com>
2025-11-12 15:04:13 -0300

    1

--- lib/textProcessor.ts
+++ lib/textProcessor.ts
@@ -28,9 +28,10 @@ Text: {TEXT}
     case "translate-pt":
     case "translate-en":
       return `You are a translator. Translate the following text from its current language to {LANGUAGE}. Output ONLY the translated text with no labels or explanations.
+The result text MUST be as long as the original text!!!
+Don't skip any part of the text - or we will lose everything!!!
 
 Text to translate: {TEXT}
-
 `;
 
     default:
